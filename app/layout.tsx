8aec94e941e2abf883e06e4fe35ecadb5a2a63cc
sameep-sadotra <sadotrasameep@gmail.com>
2025-07-04 12:29:58 +0530

    Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Call Center App',
+  description: 'Created by Sameep Sadotra',
+  
 }
 
 export default function RootLayout({
